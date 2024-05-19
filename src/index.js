b70999abc1f5014896f1b45f3816b6fec110719c
Saransh-786 <97505680+Saransh-786@users.noreply.github.com>
2024-05-19 12:03:14 +0530

    apione

--- src/index.js
+++ src/index.js
@@ -6,27 +6,11 @@ dotenv.config({
     path:'./.env'
 })
 connectDB()
-
-
-
-/*
-import express from "express"
-const app=express()
-
-(async() => {
-    try{
-        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        app.on("error",()=>{
-            console.log("ERROR:",error);
-            throw error
-        })
-        app.listen(process.env.PORT,()=>{
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
-    }
-    catch(error){
-        console.error("ERROR:,error")
-        throw err
-    }
-})()
-*/
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`server is running at PORT :${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MOnGO db connection failed!!!",err);
+})
