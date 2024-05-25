405b3ee33381da19d1663db33009250e24407281
Saransh-786 <97505680+Saransh-786@users.noreply.github.com>
2024-05-26 00:17:25 +0530

    fileuploadtry-catch

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -9,3 +9,21 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLUDINARY_API_SECRET,
 });
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try{
+        if(!localFilePath)return null
+        const response=await cloudinary.uploader.upload(localFilePath,{resource_type:"auto"})
+        console.log("file is uploaded on clodinary",response.url);
+        return response;
+    }
+    catch(error){
+        fs.unlinkSync(localFilePath)
+        //remove the locally saved file from system if failed
+        return null;
+    }
+}
+
+cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" }, 
+  function(error, result) {console.log(result); });
